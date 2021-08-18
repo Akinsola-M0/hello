8728468d2cf48415e7f5d78d7907335022699836
Akinsola-M0 <olulare6@gmail.com>
2021-08-18 02:10:37 +0100

    greeting function added

---
+++ src/com/klare/Main.java
@@ -0,0 +1,18 @@
+package com.klare;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String name = "Akinsola Moses";
+
+        // calling sayHello method
+        sayHello(name);
+    }
+
+    public static void sayHello(String personName)
+    {
+        // printing out hello personName.
+        System.out.println("Hello " + personName);
+    }
+}
